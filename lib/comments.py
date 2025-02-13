a7b4a8e03a49f4574be4f9dddca08b4726c56cc2
Richard Reed <reedqa@gmail.com>
2025-02-13 11:06:42 -0600

    adding create update delete class methods and test

--- lib/comments.py
+++ lib/comments.py
@@ -1,7 +1,7 @@
 import requests
 
 from lib.utils import build_request_headers
-from config import SESSION
+from config import SESSION, LOG
 
 
 class Comments:
@@ -10,9 +10,37 @@ class Comments:
         self.comment_url = '/comments'
 
     def get_all_comments(self, app_url, access_token):
+        LOG.info('get_all_comments()')
         request_headers = build_request_headers(access_token)
         response = SESSION.get(f'{app_url}{self.comment_url}', headers=request_headers, timeout=10)
         return response
 
+    def create_comment(self, app_url, access_token, comment):
+        LOG.info('create_comment()')
+        request_headers = build_request_headers(access_token)
+        payload = {'text': comment}
+        LOG.debug(f'Payload: {payload}')
+        response = SESSION.post(f'{app_url}{self.comment_url}', headers=request_headers, params=payload, timeout=10)
+        return response
+    
+    def update_comment(self, app_url, access_token, comment_id, **kwargs):
+        LOG.info('update_comment()')
+        request_headers = build_request_headers(access_token, content_type='application/json')
+        payload = {}
+
+        if 'comment' in kwargs:
+            payload['comment_text'] = kwargs['comment']
+        if 'likes' in kwargs:
+            payload['likes'] = kwargs['likes']
+        
+        LOG.debug(f'Request payload: {payload}')
+        response = SESSION.put(f'{app_url}{self.comment_url}/{comment_id}', headers=request_headers, json=payload, timeout=10)
+        return response
+    
+    def delete_comment(self, app_url, access_token, comment_id):
+        LOG.info('delete_comment()')
+        request_headers = build_request_headers(access_token)
+        response = SESSION.delete(f'{app_url}{self.comment_url}/{comment_id}', headers=request_headers, timeout=10)
+        return response
 
     
--- tests/comments/test_comments.py
+++ tests/comments/test_comments.py
@@ -5,12 +5,39 @@ import logging
 from lib.utils import build_request_headers
 from lib.comments import Comments
 
-
 from config import APP_URL, LOG
 
 def test_get_all_comments(admin_login):
-    LOG.info('test_get_all_comments()')
     response = Comments().get_all_comments(APP_URL, admin_login)
     LOG.debug(response.json())
     assert response.ok
+
+def test_create_update_delete_comment(admin_login):
+    LOG.info('test_create_update_delete_comment()')
+
+    # Create a comment
+    comment = 'My first post'
+    response = Comments().create_comment(APP_URL, admin_login, comment)
+    assert response.ok
+    response_body = response.json()
+    comment_id = response_body['id']
+    LOG.debug(response.json())
+    assert response_body['comment_text'] == comment
+    
+    # Update the comment
+    update_comment = f'New post on id {comment_id}'
+    response = Comments().update_comment(APP_URL, admin_login, comment_id, comment=update_comment, likes=1)
+    assert response.ok
+    response_body = response.json()
+    LOG.debug(response.json())
+    assert response_body['comment_text'] ==  update_comment
+    assert response_body['likes'] == 1
+
+    # Delete the comment
+    response = Comments().delete_comment(APP_URL, admin_login, comment_id)
+    assert response.ok
+    response_body = response.json()
+    LOG.debug(response.json())
+    assert response_body['detail'] == f'Deleted comment {comment_id}'
+    
 
