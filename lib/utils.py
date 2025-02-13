34b8d2db53281b2429467daf26f23cb8a2688090
Richard Reed <reedqa@gmail.com>
2025-02-13 11:05:17 -0600

    refactor to take in kwargs

--- lib/utils.py
+++ lib/utils.py
@@ -2,9 +2,19 @@
 contains utility functions
 """
 
+from config import LOG
+from config import HideSensitiveInfoFilter
+
 # returns request headers for API requests
-def build_request_headers(access_token, content_type='application/json'):
-    return {
+def build_request_headers(access_token, accept_type='application/json', **kwargs):
+    LOG.info('build_request_headers()')
+    headers = {
         'Authorization': f'Bearer {access_token}',
-        'Accept': content_type
+        'Accept': accept_type
     }
+    if 'content_type' in kwargs:
+        headers['Content-Type'] = kwargs['content_type']
+
+    LOG.debug(f'Headers: {headers}')
+
+    return headers
